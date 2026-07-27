9afb26ba239aa418bf6b972a5cf3ec4548a4da0c
josh6627 <josh051915@gmail.com>
2026-07-27 01:37:21 -0700

    added function so PID constants can be updated while nodes are spinning

--- src/kinematics_cpp/src/position_controller.cpp
+++ src/kinematics_cpp/src/position_controller.cpp
@@ -1,5 +1,6 @@
 #include "kinematics_cpp/PID.hpp"
 #include "kinematics_cpp/srv/set_joint_target.hpp"
+#include "rcl_interfaces/msg/set_parameters_result.hpp"
 #include "rclcpp/rclcpp.hpp" // IWYU pragma: keep
 #include "sensor_msgs/msg/joint_state.hpp"
 #include "std_msgs/msg/float64.hpp"
@@ -7,6 +8,7 @@
 #include <cmath>
 #include <cstddef>
 #include <memory>
+#include <rclcpp/node_interfaces/node_parameters_interface.hpp>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -20,6 +22,11 @@ class PositionController final : public rclcpp::Node {
     PositionController() : Node("position_controller") {
         declare_parameters();
         load_parameters();
+        parameter_callback_handle_ = this->add_on_set_parameters_callback(
+            [this](const std::vector<rclcpp::Parameter> &parameters) {
+                return update_pid_parameters(parameters);
+            }
+        );
         joint_state_subscriber_ = this->create_subscription<JointState>(
             "/joint_states",
             10,
@@ -93,6 +100,52 @@ class PositionController final : public rclcpp::Node {
         }
     }
 
+    rcl_interfaces::msg::SetParametersResult
+    update_pid_parameters(const std::vector<rclcpp::Parameter> &parameters) {
+        rcl_interfaces::msg::SetParametersResult result;
+        result.successful = true;
+
+        for (const auto &parameter : parameters) {
+            for (std::size_t i = 0; i < joint_names_.size(); ++i) {
+                const std::string prefix = joint_names_[i] + ".pid.";
+
+                if (parameter.get_name() == prefix + "kp" ||
+                    parameter.get_name() == prefix + "ki" ||
+                    parameter.get_name() == prefix + "kd" ||
+                    parameter.get_name() == prefix + "kf" ||
+                    parameter.get_name() == prefix + "start_i") {
+                    const double kp = get_parameter(prefix + "kp").as_double();
+                    const double ki = get_parameter(prefix + "ki").as_double();
+                    const double kd = get_parameter(prefix + "kd").as_double();
+                    const double kf = get_parameter(prefix + "kf").as_double();
+                    const double start_i = get_parameter(prefix + "start_i").as_double();
+
+                    double new_kp = kp;
+                    double new_ki = ki;
+                    double new_kd = kd;
+                    double new_kf = kf;
+                    double new_start_i = start_i;
+
+                    if (parameter.get_name() == prefix + "kp")
+                        new_kp = parameter.as_double();
+                    else if (parameter.get_name() == prefix + "ki")
+                        new_ki = parameter.as_double();
+                    else if (parameter.get_name() == prefix + "kd")
+                        new_kd = parameter.as_double();
+                    else if (parameter.get_name() == prefix + "kf")
+                        new_kf = parameter.as_double();
+                    else if (parameter.get_name() == prefix + "start_i")
+                        new_start_i = parameter.as_double();
+
+                    joint_pids_[i].set_constants(new_kp, new_ki, new_kd, new_kf, new_start_i);
+
+                    RCLCPP_INFO(get_logger(), "Updated PID gains for %s", joint_names_[i].c_str());
+                }
+            }
+        }
+
+        return result;
+    }
     /**
      * @brief Callback to get current joint positions
      *
@@ -165,7 +218,9 @@ class PositionController final : public rclcpp::Node {
         for (std::size_t i = 0; i < joint_names_.size(); i++) {
             std_msgs::msg::Float64 effort;
             effort.data = joint_pids_[i].compute(joint_positions_[i], dt);
+            effort.data = std::clamp(effort.data, -10.0, 10.0);
             effort_publishers_[i]->publish(effort);
+            // RCLCPP_INFO(get_logger(), "%s: %.2f", joint_names_[i].c_str(), joint_pids_[i].error);
         }
     }
 
@@ -178,6 +233,7 @@ class PositionController final : public rclcpp::Node {
     rclcpp::Time previous_time_;
     bool recevied_joint_state_ = false;
 
+    rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr parameter_callback_handle_;
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Subscription<JointState>::SharedPtr joint_state_subscriber_;
     rclcpp::Service<SetJointTarget>::SharedPtr joint_targets_service_;
